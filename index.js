6858078834f299107ee1adc26024d3594cc8726a
William <william.jiang1101@gmail.com>
2025-10-07 11:05:22 -0400

    modified index.js

--- index.js
+++ index.js
@@ -2,9 +2,28 @@
 Write a function named  capitalizeWords that takes a string (sentence) as input and returns 
 a new string where the first letter of each word is capitalized.
 */
-const capitalizeWords = (sentence) =>{
- //write your solution here
+
+// capitalize the 0 index first
+// LOOP: look at each character, is it a space?
+//     IF we find a space:
+//       take note of the indexed
+//       continue to the next
+//     IF we've taken note of the previous index:
+//       capitalie the letter
+
+
+const capitalizeWords = (sentence) => {
+    //write your solution here
+    let newString = sentence[0].toUpperCase();
+    for (let i = 1; i < sentence.length; i++) {
+        if (sentence[i - 1] === ' ') {
+            newString += sentence[i].toUpperCase();
+        } else
+            newString += sentence[i];
+    }
+    return newString;
 }
+console.log(capitalizeWords("hello world from javascript"))
 
 capitalizeWords("hello world from javascript");
 // → "Hello World From Javascript"
